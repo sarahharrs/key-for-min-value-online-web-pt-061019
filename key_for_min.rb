e02469f3de01c68523086944368241aa21b3687f
Sarah Harrs <sarahharrs@gmail.com>
2019-07-06 20:15:24 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@ wedding_hash = {"gown" => 100, "veil" => 50, "flowers" => 10}
 
 def key_for_min_value(wedding_hash)
 key_for_min. collect. do |item, points|
-  points
+ lowest_key = nil
+  lowest_value = nil
+  hash.each do |item, points|
+    if lowest_value == nil || points < lowest_value
+      lowest_value = points
+      lowest_key = item
+    end
+  end
+  lowest_key
 end
